b9487ca697ebed35560484732c58dd46e40b16bb
Joaquim Bellmunt <joaquim.bell@gmail.com>
2015-04-23 17:01:51 +0800

    Getting time from the web as JSON.

---
+++ api/controllers/TimeController.js
@@ -0,0 +1,32 @@
+/**
+ * TimeController
+ *
+ * @description :: Server-side logic for managing times
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var moment = require('moment-timezone');
+
+module.exports = {
+
+    index: function(req, res, next) {
+
+
+	var year = moment().format('YYYY');	
+	var month= moment().format('MM');
+	var day = moment().format('DD');
+	var hour = moment().tz("Asia/Singapore").format('ha');
+	var min = moment().format('mm');
+	var sec = moment().format('ss');
+	
+	return res.json({
+	    year:year,
+	    month:month,
+	    day:day,
+	    hour:hour,
+	    min:min,
+	    sec:sec
+	});
+    }
+    
+};
+
